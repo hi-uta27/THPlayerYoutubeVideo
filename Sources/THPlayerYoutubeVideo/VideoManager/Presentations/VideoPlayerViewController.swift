f9e13fe363cc9831b64bac0ac2144a0ae5215856
hi-uta27 <tavanhieun@gmail.com>
2023-09-13 19:27:44 +0700

    Update VideoPlayerViewController.swift

--- Sources/THPlayerYoutubeVideo/VideoManager/Presentations/VideoPlayerViewController.swift
+++ Sources/THPlayerYoutubeVideo/VideoManager/Presentations/VideoPlayerViewController.swift
@@ -124,7 +124,7 @@ extension VideoPlayerViewController: VideoPlayerManagerDelegate {
 
 public extension VideoPlayerViewController {
     static func initial(videoID: String, thumnailURL: String) -> VideoPlayerViewController {
-        print(Self.self, #function, "Init from ", Bundle.module)
+        print(Self.self, #function, "Init from ", Bundle.module.bundleIdentifier ?? "NSBundle")
         let storyboard = UIStoryboard(name: "\(Self.self)", bundle: Bundle.module)
         let viewController = storyboard.instantiateViewController(withIdentifier: "\(Self.self)") as! VideoPlayerViewController
         viewController.videoID = videoID
