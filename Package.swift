5c28de5fd2f1c80ee8bc6f41a026da0514a467f2
hi-uta27 <tavanhieun@gmail.com>
2023-09-13 16:46:29 +0700

    update

--- Package.swift
+++ Package.swift
@@ -23,8 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "THPlayerYoutubeVideo",
-            dependencies: ["SDWebImage", "XCDYouTubeKit"],
-            path: "Source"),
+            dependencies: ["SDWebImage", "XCDYouTubeKit"]),
         .testTarget(
             name: "THPlayerYoutubeVideoTests",
             dependencies: ["THPlayerYoutubeVideo"]),
